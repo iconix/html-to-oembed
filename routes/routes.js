1dd7f76fbdd8ef88cb2681774b140c84e9234201
iconix <narho@microsoft.com>
2017-03-03 15:56:04 -0800

    Add post route that expects a req body with json that has an "html" property

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,22 @@
 var appRouter = function(app) {
+    var rootRoute = "/";
 
-    app.get("/", function(req, res) {
+    app.get(rootRoute, function(req, res) {
         var url = req.query.url;
-
-        // TODO accept HTML in the request body as well
 
         if (!url) {
             return res.status(400).send({ status: 400, message: "bad request: missing url" });
         } else {
             return res.send("request to embed url " + url);
+        }
+    });
+
+    app.post(rootRoute, function(req, res) {
+        if (!req.body) {
+            return res.status(400).send({ status: 400, message: "bad request: missing request body" });
+        } else {
+            var html = req.body.html;
+            return res.send("request to embed html " + html);
         }
     });
 }
